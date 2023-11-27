5a513985f2ce481b62d47946fa3376ce5017a3e6
aysenur <aaysenurcevik@gmail.com>
2023-11-27 16:42:32 +0300

    ZZ Tattoo Website is implemented.

---
+++ clothes.php
@@ -0,0 +1,300 @@
+
+<?php
+//index.php
+
+?>
+
+
+<!doctype html>
+
+<html lang= "en">
+<head>
+    <meta charset= "utf-8">
+    <meta name= "description" content="Eryaman Dövme Stüdyosu">
+    <meta name= "author" content="ZZ Tattoo">
+    <meta name= "keywords" content="Tattoo, Dövme, Tasarım, Piercing, Eryaman Tattoo, Eryaman, Eryaman Dövme, Ankara, Ankara Dövme, Ankara Tattoo">
+    <meta name= "viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title> ZZ TATTOO - Hakkımızda</title>
+    
+    <link rel= "stylesheet" type="text/css" href= "css/font-awesome.min.css">
+        
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <link rel= "stylesheet" type="text/css" href= "css/reset.css">
+    <link rel= "stylesheet" type="text/css" href= "css/main.css">
+
+
+    </head>
+    
+    <body>
+        
+        <header>
+        <div class="container2">
+            <div class="logo">
+            <a href="index.html"> <img src="img/logo.jpeg">
+                </a>
+            </div>
+            <div class="menu">
+            <ul>
+                <li><a href="index.html">ANASAYFA</a> </li>
+                <li><a href="about.html">HAKKIMIZDA</a> </li>
+                <li><a href="clothes.php">GİYİM</a></li>                
+                <li><a href="tattooArtist.html">DÖVME SANATÇILARI</a> </li>
+                <li><a href="gallery.html">GALERİ</a> </li>
+                <li><a href="contact.html">İLETİŞİM</a> </li>
+                <li><a href="order_process.php"><i class="fa fa-shopping-cart"></i> SEPET</a>
+
+                </ul>
+            </div>
+            </div>
+        </header>
+        
+ <!--  MAIN SECTİON -->
+        
+<!--
+        <section id="aboutSlider" class="slider">
+            <div id="aboutCaption" class="caption">
+
+            </div>
+        </section>
+-->
+               
+        
+<!--  HAKKIMIZDA -->
+        
+        <section id="social" class="sectionArea">
+            <div class="socialTop">
+                <h2 class="sectionHeader">GİYİM</h2>
+
+<body>
+  <div class="container">
+   <br />
+   <nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+     <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="sr-only">Menu</span>
+      <span class="glyphicon glyphicon-menu-hamburger"></span>
+      </button>
+      <a class="navbar-brand" href="/">Sepet</a>
+     </div>
+     <div id="navbar-cart" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+       <li>
+        <a id="cart-popover" class="btn" data-placement="bottom" title="Alışveriş Sepeti">
+         <span class="glyphicon glyphicon-shopping-cart"></span>
+            <span class="badge"></span>
+         <span class="total_price">TL0.00</span>
+        </a>
+       </li>
+      </ul>
+     </div>
+    </div>
+   </nav>
+   
+   <div id="popover_content_wrapper" style="display: none">
+    <span id="cart_details"></span>
+    <div align="right">
+     <a href="order_process.php" class="btn btn-primary" id="check_out_cart">
+      <span class="glyphicon glyphicon-shopping-cart"></span> Satın Al
+     </a>
+     <a href="#" class="btn btn-default" id="clear_cart">
+      <span class="glyphicon glyphicon-trash"></span> Sil
+     </a>
+    </div>
+   </div>
+
+   <div id="display_item" class="row">
+
+   </div>
+
+    
+    <br />
+    <br />
+   </div>
+</body>
+
+         
+            </div>         
+        </section>
+
+        
+<!--  SOCIAL MEDIA -->
+        
+        <section id="social" class="sectionArea">
+            <div class="socialTop">
+         
+            </div>
+            <div class="socialBody">
+            <div class="container2">
+
+                
+                  <div class="col3">
+                <div class="item">
+                    <div class="zoom"> </div>
+
+                        <div class="workHour">  
+                            <h3> ÇALIŞMA SAATLERİ</h3>
+                           <h33> <p>Pzt:  09:00–22:00</p></h33>
+                           <h33> <P>Sal:  09:00–22:00</P></h33>
+                           <h33> <P>Çar:  09:00–22:00</P></h33>
+                           <h33> <P>Per:  09:00–22:00</P></h33>
+                           <h33> <P>Cum:  09:00–22:00</P></h33>
+                           <h33> <P>Cmt:  09:00–22:00</P></h33>
+                           <h33> <P>Paz:  09:00–22:00</P></h33>                         
+
+                    </div>                                         
+                     </div>
+                    </div>
+                                    
+                    <div class="col3">
+                <div class="item">
+                    <div class="zoom"> 
+                            <p> <a href="https://www.google.com/maps/dir//'39.9783678,32.6182936'/@39.9780139,32.5482116,12z/data=!4m6!4m5!1m0!1m3!2m2!1d32.6182936!2d39.9783678" class="btn3"> <img src="img/konum.png"> </a>  </p>
+
+                    </div>
+                        <div class="itemText">
+    
+                                              
+             
+                        </div>
+                    </div>
+                    </div>
+                
+                                  <div class="col3">
+                <div class="item">
+                    <div class="zoom"> </div>
+
+                        <div class="itemText">  
+                 <div class="lınks">
+                <ul>
+                    <li><h6><a href="https://www.google.com/maps/dir//'39.9783678,32.6182936'/@39.9780139,32.5482116,12z/data=!4m6!4m5!1m0!1m3!2m2!1d32.6182936!2d39.9783678"><i class="fa fa-map-marker "></i> Eryaman, akcenter avm, D:6/80, 262. Sk. 06824 Etimesgut/Ankara Türkiye</a> </h6> </li>
+                </ul>
+                        
+                    </div>                         
+
+                    </div>                                         
+                     </div>
+                    </div>
+                </div>
+                </div>
+            
+        </section>
+        
+
+        
+<!--   CALL and WORKSECTION  -->
+        
+        
+        <section id="call" class="sectionArea">
+        <div class="callTop">
+<!--            <h2 class="sectionHeader">HEMEN ARA</h2>-->
+
+            </div> 
+             <div class="callBody">
+            <div class="container2">
+                <div class="col2">
+                <div class="callItem">
+                    <div class="zoom">  </div>
+
+                        <div class="itemText">
+                  <a href="index.html"> <img src="img/logo.jpeg"></a>  
+
+             
+                        </div>
+                    </div>
+                    </div>   
+                <div class="col2">
+                <div class="item">
+                    <div class="zoom"> </div>
+
+                        <div class="itemText">
+
+            <ul>
+                <li><a href="index.html">ANASAYFA</a> </li>
+                <li><a href="about.html">HAKKIMIZDA</a> </li>
+                <li><a href="clothes.php">GİYİM</a></li>                
+                <li><a href="tattooArtist.html">DÖVME SANATÇILARI</a> </li>
+                <li><a href="gallery.html">GALERİ</a> </li>
+                <li><a href="contact.html">İLETİŞİM</a> </li>
+
+                </ul>    
+                      </div>
+                    </div>
+                    </div>
+                            <div class="col2">
+                <div class="item">
+                    <div class="zoom"> </div>
+                        <div class="itemText">
+                            <h4> BİZE ULAŞIN</h4>
+                            
+                                                        
+                  <div class="lınks">
+                <ul>
+                    <li><a href="tel:+90-530-624-69-06"><i class="fa fa-phone "></i> TELEFON</a> </li>
+                <li><a href="https://www.instagram.com/doguakyel_tattoo/"><i class="fa fa-instagram"></i> INSTAGRAM</a> </li>
+                <li><a href="https://www.facebook.com/profile.php?id=100015248985105"><i class="fa fa-facebook "></i> FACEBOOK</a> </li>
+                <li><a href="https://www.youtube.com/channel/UCvbV8Tq6SkRsRF6Brce7uBw?view_as=subscriber"><i class="fa fa-youtube "></i> YOUTUBE</a> </li>
+
+                 </ul>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>                                         
+                                       
+        </section>
+    </body>
+
+</html>
+
+
+<script>  
+$(document).ready(function(){
+
+ load_product();
+
+ load_cart_data();
+
+ function load_product()
+ {
+  $.ajax({
+   url:"fetch_item.php",
+   method:"POST",
+   success:function(data)
+   {
+    $('#display_item').html(data);
+   }
+  })
+ }
+
+ function load_cart_data()
+ {
+  $.ajax({
+   url:"fetch_cart.php",
+   method:"POST",
+   dataType:"json",
+   success:function(data)
+   {
+    $('#cart_details').html(data.cart_details);
+    $('.total_price').text(data.total_price);
+    $('.badge').text(data.total_item);
+   }
+  })
+ }
+
+ $('#cart-popover').popover({
+  html : true,
+  container : 'body',
+  content:function(){
+   return $('#popover_content_wrapper').html();
+  }
+ });
+    
+});
+
+</script>
